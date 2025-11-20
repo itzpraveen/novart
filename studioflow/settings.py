1ef2187c22113b6c21b994e74fc84ae7222435f5
itzpraveen <itzpraveen@users.noreply.github.com>
2025-11-20 23:48:22 +0530

    Handle empty DATABASE_URL and pin Python version

--- studioflow/settings.py
+++ studioflow/settings.py
@@ -81,8 +81,9 @@ WSGI_APPLICATION = 'studioflow.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+db_url = os.environ.get('DATABASE_URL') or f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
 DATABASES = {
-    'default': dj_database_url.config(default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}", conn_max_age=600),
+    'default': dj_database_url.parse(db_url, conn_max_age=600),
 }
 
 
