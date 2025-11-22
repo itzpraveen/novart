ebed195ac593b9d042f2001f359adf6e14061b08
itzpraveen <itzpraveen@users.noreply.github.com>
2025-11-22 12:49:09 +0530

    Use Noto Sans for rupee-safe invoice PDFs

--- portal/views.py
+++ portal/views.py
@@ -447,7 +447,7 @@ def invoice_pdf(request, invoice_pk):
         fallback_logo = finders.find('img/novart.png')
         logo_data = _encode_image(fallback_logo)
 
-    font_path = finders.find('fonts/DejaVuSans.ttf')
+    font_path = finders.find('fonts/NotoSans-Regular.ttf') or finders.find('fonts/DejaVuSans.ttf')
     html = render_to_string(
         'portal/invoice_pdf.html',
         {
